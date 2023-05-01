8b1d82dc10a2d24321294d900fff18d97e1e29d2
Walker Spence <walker.spence@gmail.com>
2023-05-01 09:12:00 -0700

    cleaned up proof of concept

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,74 @@
+import type { NextRouter } from 'next/router';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
-/* eslint-disable */
+type AlertData = { status: string | undefined; message: string | undefined };
 
-const Index = () => {
-  const router = useRouter();
-  const [alertData, setAlertData] = useState<{
-    status: string | undefined;
-    message: string | undefined;
-  }>({
-    status: undefined,
-    message: undefined,
-  });
+const storeAndDeleteQueryAlert = (router: NextRouter) => {
   const { query } = router;
   const { error, warning, success } = query;
-  const errorMessage = Array.isArray(error) ? error[0] : error;
-  const warningMessage = Array.isArray(warning) ? warning[0] : warning;
-  const successMessage = Array.isArray(success) ? success[0] : success;
-  const queryMessage = errorMessage || warningMessage || successMessage;
+  const alertData: AlertData = { status: undefined, message: undefined };
 
-  if (queryMessage) {
-    if (successMessage) {
-      localStorage.setItem('success', successMessage);
-    } else if (errorMessage) {
-      localStorage.setItem('error', errorMessage);
-    } else if (warningMessage) {
-      localStorage.setItem('warning', warningMessage);
+  const alertMessage = success || error || warning;
+  if (alertMessage) {
+    alertData.message =
+      alertMessage && Array.isArray(alertMessage)
+        ? alertMessage[0]
+        : alertMessage;
+    if (success) {
+      alertData.status = 'success';
+    } else if (error) {
+      alertData.status = 'error';
+    } else if (warning) {
+      alertData.status = 'warning';
     }
+
+    localStorage.setItem('alertData', JSON.stringify(alertData));
 
     delete query.success;
     delete query.error;
     delete query.warning;
-    router.replace({ query });
+    router.replace({ query }, undefined, { shallow: true });
   }
+};
+
+/**
+ * Uses React hooks to manage the `alertData` state. It first calls the
+ * `storeAndDeleteQueryAlert` function to store any query parameters in local storage
+ * and remove them from the URL. It then uses `useEffect` to retrieve the `alertData`
+ * object from local storage and set it as the state value using `setAlertData`.
+ * Finally, it removes the `alertData` object from local storage and returns the `alertData`
+ * state value.
+ * @returns {AlertData} The `alertData` object containing the status and message of the alert.
+ */
+
+const useAndStoreAlert = () => {
+  const router = useRouter();
+  const [alertData, setAlertData] = useState<AlertData>({
+    status: undefined,
+    message: undefined,
+  });
+
+  storeAndDeleteQueryAlert(router);
 
   useEffect(() => {
-    const storedSuccess = localStorage.getItem('success');
-    const storedWarning = localStorage.getItem('warning');
-    const storedError = localStorage.getItem('error');
-    if (storedSuccess) {
-      setAlertData({ status: 'success', message: storedSuccess });
-      localStorage.removeItem('success');
-    } else if (storedError) {
-      setAlertData({ status: 'error', message: storedError });
-      localStorage.removeItem('error');
-    } else if (storedWarning) {
-      setAlertData({ status: 'warning', message: storedWarning });
-      localStorage.removeItem('warning');
+    const alertDataJson = localStorage.getItem('alertData');
+    if (alertDataJson) {
+      setAlertData(JSON.parse(alertDataJson));
     }
+    localStorage.removeItem('alertData');
   });
+
+  return alertData;
+};
+
+const Index = () => {
+  const router = useRouter();
+
+  const { status, message } = useAndStoreAlert();
 
   return (
     <Main
@@ -62,31 +79,38 @@ const Index = () => {
         />
       }
     >
-      {alertData.status || 'none!'}, {alertData.message || 'none!'}
+      {/* eslint-disable react/button-has-type */}
       <div>
         <button
           onClick={() => {
-            router.push({ query: { warning: 'mycustomwarning' } });
+            router.push({ query: { warning: 'this is a warning 🥵' } });
           }}
         >
-          Warning
+          Click for Warning
         </button>
         <br />
         <button
           onClick={() => {
-            router.push({ query: { error: 'mycustomerror' } });
+            router.push({ query: { error: 'something went wrong 🚨' } });
           }}
         >
-          Error
+          Click for Error
         </button>
         <br />
         <button
           onClick={() => {
-            router.push({ query: { success: 'mycustomsuccess' } });
+            router.push({ query: { success: 'everything is okay 🥳' } });
           }}
         >
-          Success
+          Click for Success
         </button>
+      </div>
+      <hr />
+      <hr />
+      <div>
+        status: {status || 'none!'}
+        <br />
+        message: {message || 'none!'}
       </div>
     </Main>
   );
